90acb811c58bf095a32de0b2cc6ceebafc3a9088
Anas <anasna6005@gmail.com>
2024-07-15 22:29:37 +0530

    Changed vercel file

--- client/src/utils/Axios/Endpoints/index.js
+++ client/src/utils/Axios/Endpoints/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://trika-server.vercel.app/"
+const BASE_URL = "https://trika-server.vercel.app"
 
 export const SEARCH_URL = `${BASE_URL}/search`;
 export const GET_PRODUCTS_URL = `${BASE_URL}/products`;
